c1911ea94c167ba64a004101164ba1eb2b5f09de
santokan <houska.peter@gmail.com>
2024-09-10 15:13:11 +0200

    started working on main

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	fmt.Printf("starting crawl of: %s\n", os.Args[1])
 }

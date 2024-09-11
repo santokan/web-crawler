7eddb42fa6cbde53c233c707180e4b1cb254ea02
santokan <houska.peter@gmail.com>
2024-09-11 16:25:30 +0200

    added maxPages limit

--- main.go
+++ main.go
@@ -3,21 +3,34 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("no website provided")
+	if len(os.Args) < 4 {
+		fmt.Println("not enough arguments provided")
+		fmt.Println("usage: crawler <baseURL> <maxConcurrency> <maxPages>")
 		return
 	}
-	if len(os.Args) > 2 {
+	if len(os.Args) > 5 {
 		fmt.Println("too many arguments provided")
+		fmt.Println("usage: crawler <baseURL> <maxConcurrency> <maxPages>")
 		return
 	}
 	rawBaseURL := os.Args[1]
 
-	const maxConcurrency = 3
-	cfg, err := configure(rawBaseURL, maxConcurrency)
+	maxConcurrency, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("Error - maxConcurrency: %v", err)
+		return
+	}
+
+	maxPages, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Printf("Error - maxPages: %v", err)
+	}
+
+	cfg, err := configure(rawBaseURL, maxConcurrency, maxPages)
 	if err != nil {
 		fmt.Printf("Error - configure: %v", err)
 		return
